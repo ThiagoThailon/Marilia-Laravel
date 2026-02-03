eb1c4c100bbbcf30bc38abb379c56f25bea3905f
ThiagoThailon <thiagothailons@gmail.com>
2026-02-03 11:31:41 -0300

    fav icon

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -20,7 +20,7 @@ class Ticket extends Model
     ];
     public function user()
     {
-    return $this->belongsTo(User::class);
+    return $this->belongsTo(User::class);  
 
     }
     protected $casts = [
